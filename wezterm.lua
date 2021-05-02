57bf2b2c240f117f0bf691ae388c43d6a055bbfe
David Bradford <david.bradford@mongodb.com>
2021-05-01 22:43:43 -0400

    More wezterm keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -18,6 +18,15 @@ return {
         {key="t", mods="LEADER", action=wezterm.action{SpawnTab="DefaultDomain"}},
         {key="\\", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
         {key="-", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
+        {key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
+        {key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
+        {key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="h", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+        {key="j", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+        {key="k", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+        {key="l", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+
         {key="a", mods="LEADER|CTRL", action=wezterm.action{SendString="\x01"}},
     }
 }
