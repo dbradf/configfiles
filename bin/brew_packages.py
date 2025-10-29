9ea0801df52357ee665097e8e23f7827cbe59ec7
David Bradford <dbradf@gmail.com>
2025-10-29 10:10:34 -0400

    update brew packages

--- bin/brew_packages.py
+++ bin/brew_packages.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 import os
 
-PACKAGES=[
-    'atuin',
-    'bat',
-    'eza',
-    'git-delta',
-    'go',
-    'htop',
-    'hyperfine',
-    'jj',
-    'jq',
-    'neovim',
-    'nvm',
-    'raycast',
-    'rectangle',
-    'tmux',
-    'tmuxp',
-    'tree',
-    'wezterm',
-    'zsh-vi-mode',
-    'zsh-autosuggestions',
-    'zoxide',
+PACKAGES = [
+    "atuin",
+    "bat",
+    "eza",
+    "git-delta",
+    "go",
+    "htop",
+    "hyperfine",
+    "jj",
+    "jq",
+    "neovim",
+    "nvm",
+    "rectangle",
+    "tmux",
+    "tree",
+    "wezterm",
+    "zsh-vi-mode",
+    "zsh-autosuggestions",
+    "zoxide",
 ]
 
 CASKS = [
-    'ghostty',
+    "ghostty",
 ]
+
 
 def main():
     for package in PACKAGES:
-        os.system(f'brew install {package}')
+        os.system(f"brew install {package}")
 
     for cask in CASKS:
-        os.system(f'brew install --cask {cask}')
+        os.system(f"brew install --cask {cask}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
